fe67095bc32f218c50cb67ff521f25d4d55fa1a9
akyarv0 <akyarv0@gmail.com>
2024-03-21 23:21:42 +0300

    buttons

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,13 @@
+import Buton from "../buttons/Buton";
+
 const Card = ({ lang, img }) => {
     return (
       <div>
         <div className="wrapper">
           <h1>{lang} </h1>
         </div>
-        <img src={img} alt={lang} />
+        <img className= "images" src={img} alt={lang} />
+        <Buton />
       </div>
     );
   };
